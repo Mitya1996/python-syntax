bf18c9d4a1891e61b9fb03403a64787451a5ea04
mits <dmitrybaycharov@gmail.com>
2021-03-22 20:20:16 -0500

    Step One: Working in Python Files finished

--- convert.py
+++ convert.py
@@ -16,6 +16,20 @@ def convert_temp(unit_in, unit_out, temp):
     """
 
     # YOUR CODE HERE
+    if unit_in != "f" and unit_in != "c": #error handling
+	    return f"invalid unit {unit_in}"
+
+    if unit_out != "f" and unit_out != "c": #error handling
+	    return f"invalid unit {unit_out}"
+
+    if unit_in == "c" and unit_out == "f":
+        return temp * (9/5) + 32
+
+    if unit_in == "f" and unit_out == "c":
+	    return (temp - 32) * (5/9)
+
+    return temp
+
 
 
 print("c", "f", 0, convert_temp("c", "f", 0), "should be 32.0")
